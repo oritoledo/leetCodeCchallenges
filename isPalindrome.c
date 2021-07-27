3eb39987728eb4cb78db4791ab642bffafbbc3e0
Ori Toledo <74354707+oritoledo@users.noreply.github.com>
2021-07-27 09:52:52 +0200

    check if an int is a palindrome

---
+++ isPalindrome.c
@@ -0,0 +1,21 @@
+// importing necessary libraries::
+#include <stdlib.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdbool.h>
+
+bool isPalindrome(int x){
+    if (x<0) return false;
+    int r = 0;
+    int tmp = x;
+    while (tmp !=0){
+        int rem;
+        rem = tmp % 10;
+        if (r > INT_MAX / 10 || (r == INT_MAX / 10 && rem > 7)) return 0;
+        r = r * 10 + rem;
+        tmp /= 10;
+    }
+
+    if (x == r)return true;
+    return false;
+}
